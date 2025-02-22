82d40040808ec2c980baf8fbc7fe5a4bbe0483b2
hawkaii <parthibmukherjee@gmail.com>
2025-02-22 11:57:48 +0530

    added dependencies 	- httprouter 	- alice made necessary changes to comply

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,13 +7,11 @@ import (
 	"strconv"
 
 	"github.com/hawkaii/snippetbox/internal/models"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
 
 	snippets, err := app.snippets.Latest()
 	if err != nil {
@@ -29,7 +27,10 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.Atoi(params.ByName("id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -53,12 +54,10 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
+	w.Write([]byte("Create a new snippet..."))
+}
 
-	}
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 
 	title := "0 snail"
 	content := "0 snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
@@ -70,6 +69,6 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module github.com/hawkaii/snippetbox
 go 1.22.5
 
 require (
+	github.com/go-sql-driver/mysql v1.8.1
+	github.com/justinas/alice v1.2.0
+)
+
+require (
 	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
 )
